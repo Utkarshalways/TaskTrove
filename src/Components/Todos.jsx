b1adb117089988564c079a43c2f2eddae2db72c5
Utkarshalways <utkarshshrma08@gmail.com>
2024-03-23 18:12:55 +0530

    all the things are working properly

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -25,23 +25,25 @@ const Todos = ({todo}) => {
   return (
     <div
       className={
-        "  w-full flex justify-center gap-6 p-4 bg-slate-600 rounded-lg  " 
-        // (complete ? "bg-green-500  " : "")
+        "  w-full flex justify-center gap-6 p-4  rounded-lg  " +
+        (todo.complete ? "bg-green-500 " : "bg-slate-600")
       }
     >
-      <input type="checkbox" 
-      value={todo.complete} 
+      <input
+        type="checkbox"
+        className=" p-20 "
+        value={todo.complete}
         onChange={togglecompleted}
       />
       <input
         type="text"
         className={
-          " w-2/3  placeholder:text-center rounded-lg  " 
-          // (complete ? " line-through " : "")
+          " w-2/3  placeholder:text-center rounded-lg  " +
+          (todo.complete ? " line-through " : "")
         }
         value={todomsg}
         readOnly={!Editable}
-        onChange={(e)=> setTodomsg(e.target.value)}
+        onChange={(e) => setTodomsg(e.target.value)}
       />
 
       <button
@@ -49,13 +51,16 @@ const Todos = ({todo}) => {
         onClick={(e) => {
           setEditable(!Editable);
         }}
-
       >
-        Edit
+        {!Editable ? (
+          <span className="material-symbols-outlined text-black ">edit</span>
+        ) : (
+          <span className="material-symbols-outlined text-black ">save</span>
+        )}
       </button>
       <button
         className=" border-2 bg-blue-400 p-2 rounded-lg hover:bg-blue-500 text-white "
-        onClick={() => deleteTodo(todo.id) }
+        onClick={() => deleteTodo(todo.id)}
       >
         Delete
       </button>
