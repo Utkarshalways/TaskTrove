15bae77747f97061902fba8ad335559a66328157
Utkarshalways <utkarshshrma08@gmail.com>
2024-03-25 08:33:01 +0530

    App css added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Todoinput from "./Components/Todoinput";
 import { TodoProvider } from "./context";
 import Todos from "./Components/Todos";
 import { useState,useEffect } from "react";
+import "./Style/App.scss";
 
 
 function App() {
@@ -66,18 +67,18 @@ function App() {
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo,togglecomplete}}>
       {/* this div is the base of the todo */}
-      <div className=" bg-violet-700 w-full h-screen flex justify-center  items-center flex-col gap-4 ">
-        <div className=" bg-slate-600 rounded-lg p-4 text-white w-2/3 text-center font-mono flex flex-col gap-6">
+      <div className="base">
+        <div className="inputbox">
           {/* this div will contain the input part in it */}
           <p>Empower Your Day: One Task at a Time!</p>
           <Todoinput />
         </div>
 
-        <div className="flex flex-col gap-6 w-2/3 ">
+        <div className="todos">
           {/* this div will contain all the todos inside it , it will apply loop to iterate all the todos */}
           {
             todos.map((todo)=>{
-              return  <div key = { todo.id }>
+              return  <div className="eachtodo" key = { todo.id }>
                   <Todos todo={todo}/>
                 </div>
               
